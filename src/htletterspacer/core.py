f4a25bbe1e943504270f0844f54780bf07d9dfa5
Nikolaus Waxweiler <madigens@gmail.com>
2020-11-15 22:41:22 +0000

    Get basic test to work

--- src/htletterspacer/core.py
+++ src/htletterspacer/core.py
@@ -2,8 +2,13 @@ import math
 from dataclasses import dataclass
 from typing import Any, List, Optional, Tuple
 
+import fontTools.misc.bezierTools as bezierTools
+import fontTools.misc.arrayTools as arrayTools
+import fontTools.pens.basePen as basePen
 import numpy as np
 from ufoLib2.objects import Glyph
+from ufoLib2.objects.misc import BoundingBox
+from ufoLib2.objects.point import Point
 
 # Default parameters
 paramArea = 400  # white area in thousand units
@@ -26,29 +31,37 @@ def NSMakePoint(x: float, y: float) -> NSPoint:
     return NSPoint(x, y)
 
 
-@dataclass
-class NSRect:
-    __slots__ = "x", "y", "width", "height"
-    x: float
-    y: float
-    width: float
-    height: float
+# @dataclass
+# class NSRect:
+#     __slots__ = "x", "y", "width", "height"
+#     x: float
+#     y: float
+#     width: float
+#     height: float
 
 
-def NSMinX(r: NSRect) -> float:
-    return r.x
+def NSMinX(r: Optional[BoundingBox]) -> float:
+    if r is None:
+        raise ValueError("`r` is None!")
+    return r.xMin
 
 
-def NSMinY(r: NSRect) -> float:
-    return r.y
+def NSMinY(r: Optional[BoundingBox]) -> float:
+    if r is None:
+        raise ValueError("`r` is None!")
+    return r.yMin
 
 
-def NSMaxX(r: NSRect) -> float:
-    return r.x + r.width
+def NSMaxX(r: Optional[BoundingBox]) -> float:
+    if r is None:
+        raise ValueError("`r` is None!")
+    return r.xMax
 
 
-def NSMaxY(r: NSRect) -> float:
-    return r.y + r.height
+def NSMaxY(r: Optional[BoundingBox]) -> float:
+    if r is None:
+        raise ValueError("`r` is None!")
+    return r.yMax
 
 
 class HTLetterspacerLib:
@@ -64,6 +77,7 @@ class HTLetterspacerLib:
         self.RSB = RSB
         self.factor = factor
         self.width = width
+        self.newWidth = 0.0
 
     def overshoot(self):
         return self.xHeight * self.paramOver / 100
@@ -213,18 +227,14 @@ class HTLetterspacerLib:
         overshoot = self.overshoot()
 
         # store min and max y
-        self.minYref = NSMinY(referenceLayer.bounds) - overshoot
-        self.maxYref = NSMaxY(referenceLayer.bounds) + overshoot
+        self.minYref = NSMinY(referenceLayer.getBounds()) - overshoot
+        self.maxYref = NSMaxY(referenceLayer.getBounds()) + overshoot
 
         # bounds
         lFullMargin, rFullMargin = marginList(layer)
 
-        lMargins = filter(
-            lambda p: p.y >= self.minYref and p.y <= self.maxYref, lFullMargin
-        )
-        rMargins = filter(
-            lambda p: p.y >= self.minYref and p.y <= self.maxYref, rFullMargin
-        )
+        lMargins = [p for p in lFullMargin if self.minYref <= p.y <= self.maxYref]
+        rMargins = [p for p in rFullMargin if self.minYref <= p.y <= self.maxYref]
 
         # create a closed polygon
         lPolygon, rPolygon = self.processMargins(lMargins, rMargins)
@@ -236,7 +246,9 @@ class HTLetterspacerLib:
 
         # get extreme points deitalized
         lFullExtreme, rFullExtreme = self.maxPoints(
-            lFullMargin + rFullMargin, NSMinY(layer.bounds), NSMaxY(layer.bounds)
+            lFullMargin + rFullMargin,
+            NSMinY(layer.getBounds()),
+            NSMaxY(layer.getBounds()),
         )
         # get zone extreme points
         lExtreme, rExtreme = self.maxPoints(
@@ -285,6 +297,9 @@ class HTLetterspacerLib:
     def spaceMain(
         self, layer: Glyph, referenceLayer: Glyph
     ) -> Tuple[Optional[List[NSPoint]], Optional[List[NSPoint]]]:
+        # TODO: decompose glyphs
+        assert not layer.components
+
         lp, rp = None, None
         self.output = ""
         if not layer.name:
@@ -329,8 +344,8 @@ class HTLetterspacerLib:
 def setSidebearings(
     layer: Glyph, newL: float, newR: float, width: float, color: Any
 ) -> None:
-    layer.LSB = newL
-    layer.RSB = newR
+    layer.setLeftMargin(newL)
+    layer.setRightMargin(newR)
 
     # adjusts the tabular miscalculation
     if width:
@@ -357,32 +372,194 @@ def area(points):
 
 
 # get margins in Glyphs
-def getMargins(layer, y):
-    startPoint = NSMakePoint(NSMinX(layer.bounds), y)
-    endPoint = NSMakePoint(NSMaxX(layer.bounds), y)
+def getMargins(layer: Glyph, y: float) -> Tuple[Optional[float], Optional[float]]:
+    startPoint = NSMakePoint(NSMinX(layer.getBounds()), y)
+    endPoint = NSMakePoint(NSMaxX(layer.getBounds()), y)
 
-    result = layer.calculateIntersectionsStartPoint_endPoint_(startPoint, endPoint)
-    count = len(result)
-    if count <= 2:
+    result = sorted(intersections(layer, startPoint, endPoint))
+    if not result:
         return (None, None)
 
-    left = 1
-    right = count - 2
-    return (result[left].pointValue().x, result[right].pointValue().x)
+    left = 0
+    right = -1
+    return (result[left][0], result[right][0])
 
 
 # a list of margins
-def marginList(layer):
-    y = NSMinY(layer.bounds)
+def marginList(layer: Glyph) -> Tuple[List[Any], List[Any]]:
+    y = NSMinY(layer.getBounds())
     listL = []
     listR = []
     # works over glyph copy
-    cleanLayer = layer.copyDecomposedLayer()
-    while y <= NSMaxY(layer.bounds):
-        lpos, rpos = getMargins(cleanLayer, y)
+    # cleanLayer = layer.copyDecomposedLayer()
+    while y <= NSMaxY(layer.getBounds()):
+        # lpos, rpos = getMargins(cleanLayer, y)
+        lpos, rpos = getMargins(layer, y)
+        if y <= 0:
+            print(lpos, rpos)
         if lpos is not None:
             listL.append(NSMakePoint(lpos, y))
         if rpos is not None:
             listR.append(NSMakePoint(rpos, y))
         y += paramFreq
     return listL, listR
+
+
+####
+
+
+# TODO: skia pathops also has a SegmentPenIterator
+def segments(contour: List[Point]) -> Any:
+    if not contour:
+        return []
+    segments = [[]]
+    lastWasOffCurve = False
+    for point in contour:
+        segments[-1].append(point)
+        if point.segmentType is not None:
+            segments.append([])
+        lastWasOffCurve = point.segmentType is None
+    if len(segments[-1]) == 0:
+        del segments[-1]
+    if lastWasOffCurve:
+        lastSegment = segments[-1]
+        segment = segments.pop(0)
+        lastSegment.extend(segment)
+    elif segments[0][-1].segmentType != "move":
+        segment = segments.pop(0)
+        segments.append(segment)
+    return segments
+
+
+def intersections(layer: Glyph, startPoint: NSPoint, endPoint: NSPoint) -> List[Any]:
+    intersections = []
+    x1, y1, x2, y2 = startPoint.x, startPoint.y, endPoint.x, endPoint.y
+    for contour in layer.contours:
+        for segment_pair in arrayTools.pairwise(segments(contour.points)):
+            last, curr = segment_pair
+            curr_type = curr[-1].type
+            if curr_type == "line":
+                i = lineIntersection(x1, y1, x2, y2, last[-1], curr[-1])
+                if i is not None:
+                    intersections.append(i)
+            elif curr_type == "curve":
+                i = curveIntersections(
+                    x1, y1, x2, y2, last[-1], curr[0], curr[1], curr[2]
+                )
+                if i is not None:
+                    intersections.extend(i)
+            elif curr_type == "qcurve":
+                i = qcurveIntersections(x1, y1, x2, y2, last[-1], *curr)
+                if i is not None:
+                    intersections.extend(i)
+            else:
+                raise ValueError(f"Cannot deal with segment type {curr_type}")
+    return intersections
+
+
+def curveIntersections(x1, y1, x2, y2, p1, p2, p3, p4):
+    """
+    Computes intersection between a line and a cubic curve.
+    Adapted from: https://www.particleincell.com/2013/cubic-line-intersection/
+    Takes four scalars describing line parameters and four points describing
+    curve.
+    Returns a List of intersections in Tuples of the format (coordinate_x,
+    coordinate_y, term, subsegment_index == 0). The subsegment_index is only
+    used for quadratic curves.
+    """
+    bx, by = x1 - x2, y2 - y1
+    m = x1 * (y1 - y2) + y1 * (x2 - x1)
+    a, b, c, d = bezierTools.calcCubicParameters(
+        (p1.x, p1.y), (p2.x, p2.y), (p3.x, p3.y), (p4.x, p4.y)
+    )
+    pc0 = by * a[0] + bx * a[1]
+    pc1 = by * b[0] + bx * b[1]
+    pc2 = by * c[0] + bx * c[1]
+    pc3 = by * d[0] + bx * d[1] + m
+    r = bezierTools.solveCubic(pc0, pc1, pc2, pc3)
+    sol = []
+    for t in r:
+        if t < 0 or t > 1:
+            continue
+        s0 = ((a[0] * t + b[0]) * t + c[0]) * t + d[0]
+        s1 = ((a[1] * t + b[1]) * t + c[1]) * t + d[1]
+        if (x2 - x1) != 0:
+            s = (s0 - x1) / (x2 - x1)
+        else:
+            s = (s1 - y1) / (y2 - y1)
+        if s < 0 or s > 1:
+            continue
+        sol.append((s0, s1))
+    return sol
+
+
+def qcurveIntersections(x1, y1, x2, y2, *pts) -> List[Tuple[int, int, int, int]]:
+    """
+    Computes intersection between a quadratic spline and a line segment.
+    Adapted from curveIntersections(). Takes four scalars describing line and an
+    Iterable of points describing a quadratic curve, including the first (==
+    anchor) point. Quadatric curves are special in that they can consist of
+    implied on-curve points, which is why this function returns a
+    `subsegment_index` to associate a `t` with the correct subsegment.
+    Returns a List of intersections in Tuples of the format (coordinate_x,
+    coordinate_y, term, subsegment_index).
+    """
+
+    sol = []
+    points = [(p.x, p.y) for p in pts]
+
+    nx, ny = x1 - x2, y2 - y1
+    m = x1 * (y1 - y2) + y1 * (x2 - x1)
+    # Decompose a segment with potentially implied on-curve points into subsegments.
+    # p1 is the anchor, p2 the control handle, p3 the (implied) on-curve point in the
+    # subsegment.
+    p1 = points[0]
+    for index, (p2, p3) in enumerate(basePen.decomposeQuadraticSegment(points[1:])):
+        (ax, ay), (bx, by), (cx, cy) = bezierTools.calcQuadraticParameters(p1, p2, p3)
+        p1 = p3  # prepare for next turn
+
+        pc0 = ny * ax + nx * ay
+        pc1 = ny * bx + nx * by
+        pc2 = ny * cx + nx * cy + m
+        r = bezierTools.solveQuadratic(pc0, pc1, pc2)
+
+        for t in r:
+            if t < 0 or t > 1:
+                continue
+            sx = (ax * t + bx) * t + cx
+            sy = (ay * t + by) * t + cy
+            if (x2 - x1) != 0:
+                s = (sx - x1) / (x2 - x1)
+            else:
+                s = (sy - y1) / (y2 - y1)
+            if s < 0 or s > 1:
+                continue
+            sol.append((sx, sy))
+    return sol
+
+
+def lineIntersection(x1, y1, x2, y2, p3, p4):
+    """
+    Computes intersection between two lines.
+    Adapted from Andre LaMothe, "Tricks of the Windows Game Programming Gurus".
+    G. Bach, http://stackoverflow.com/a/1968345
+    Takes four scalars describing line parameters and two points describing
+    line.
+    Returns a List of intersections in Tuples of the format (coordinate_x,
+    coordinate_y, term, subsegment_index == 0). The subsegment_index is only
+    used for quadratic curves.
+    """
+    x3, y3 = p3.x, p3.y
+    x4, y4 = p4.x, p4.y
+    Bx_Ax = x4 - x3
+    By_Ay = y4 - y3
+    Dx_Cx = x2 - x1
+    Dy_Cy = y2 - y1
+    determinant = -Dx_Cx * By_Ay + Bx_Ax * Dy_Cy
+    if abs(determinant) < 1e-12:
+        return None
+    s = (-By_Ay * (x3 - x1) + Bx_Ax * (y3 - y1)) / determinant
+    t = (Dx_Cx * (y3 - y1) - Dy_Cy * (x3 - x1)) / determinant
+    if 0 <= s <= 1 and 0 <= t <= 1:
+        return (x3 + (t * Bx_Ax), y3 + (t * By_Ay))
+    return None
--- tests/test_spacer.py
+++ tests/test_spacer.py
@@ -3,12 +3,25 @@ import ufoLib2
 
 
 def test_spacer(datadir):
-    ufo = ufoLib2.Font.open(datadir / "MutatorSansBoldCondensed.ufo")
+    ufo_orig = ufoLib2.Font.open(datadir / "MutatorSansBoldCondensed.ufo")
+    glyph_O = ufo_orig["O"]
+    glyph_H = ufo_orig["H"]
+    # ufo_rspc = ufoLib2.Font.open(datadir / "Respaced.ufo")
+
     o = htletterspacer.core.HTLetterspacerLib(
-        1000, 0, ufo.info.xHeight, True, True, 1.25, 500
+        1000, 0, ufo_orig.info.xHeight, True, True, 1.25, glyph_O.width
     )
-    glyph_O = ufo["O"]
-    glyph_H = ufo["H"]
-    lp, rp = o.spaceMain(glyph_O, glyph_H)
-    assert lp
-    assert rp
+    o.paramArea = 120
+    o.paramDepth = 5
+    o.paramOver = 0
+
+    assert glyph_O.getLeftMargin() == 20
+    assert glyph_O.getRightMargin() == 20
+
+    o.spaceMain(glyph_O, glyph_H)
+    assert o.minYref == 0
+    assert o.maxYref == 800
+    assert o.newL == 13
+    assert o.newR == 13
+    assert glyph_O.getLeftMargin() == 13
+    assert glyph_O.getRightMargin() == 13
